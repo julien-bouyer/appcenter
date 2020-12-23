3425042d75738663aaf29056cd9c42cdcd957e74
Julien BOUYER <julienbouyer@gmail.com>
2020-12-23 18:26:09 +0100

    Add password encrypt Node script

---
+++ scripts/pwd-encrypt.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt');
+
+const encrypt = (password) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const pwd = bcrypt.hash(password, 8);
+      resolve(pwd);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+const args = process.argv.slice(2);
+console.log(args);
+
+if (args && args.length > 0) {
+  args.forEach(arg => {
+    encrypt(arg)
+    .then((res) => {
+      console.log(arg, res);
+    })
+    .catch(console.error);
+  })
+} else {
+  console.warn('Vous devez renseigner au moins 1 mot de passe');
+}
